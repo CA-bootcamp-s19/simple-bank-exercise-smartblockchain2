a44c90140af2c922ee5f40c41abd6b16e0bbb3a0
smartblockchain2 <57226219+smartblockchain2@users.noreply.github.com>
2020-01-16 10:46:31 -0600

    Update SimpleBank

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -13,9 +13,11 @@ contract SimpleBank {
     //
     
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
+    // use the keyword private. 
     mapping (address => uint) private balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
+    // Solidity creates a getter function when we use the keyword public 
     mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
@@ -26,9 +28,11 @@ contract SimpleBank {
     //
     
     /* Add an argument for this event, an accountAddress */
+    // I think an analogy for the keyword address is:  "Address is an instance of the actor interacting with the contract instance."
     event LogEnrolled(address accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
+    // I've seen examples of indexed... I don't know how to 
     event LogDepositMade(address accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
@@ -67,12 +71,11 @@ contract SimpleBank {
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool){
-        require(!enrolled[msg.sender], "The user is already Enrolled"); // if rule is not true, the person is enrolled.
         enrolled[msg.sender] = true;
         emit LogEnrolled(msg.sender);
         return enrolled[msg.sender];
     }
-
+   
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
     // Add the appropriate keyword so that this function can receive ether
@@ -82,11 +85,11 @@ contract SimpleBank {
     function deposit() public payable returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
-        require(enrolled[msg.sender], "The user is not Enrolled");
-        balances[msg.sender] += msg.value;
-        emit LogDepositMade(msg.sender, msg.value);
-        return balances[msg.sender];
-     }
+          require (enrolled[msg.sender] == true,"The user is not enrolled");
+          balances[msg.sender] += msg.value;
+          emit LogDepositMade(msg.sender, msg.value);
+          return balances[msg.sender];
+    }
 
     /// @notice Withdraw ether from bank
     /// @dev This does not return any excess ether sent to it
@@ -98,10 +101,10 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
-        require(withdrawAmount <= balances[msg.sender], "Withdrawal amount > current balance");
+        require(withdrawAmount <= balances[msg.sender], "Widthdraw Amount is greater than account balance.");
         balances[msg.sender] -= withdrawAmount;
+        msg.sender.transfer(withdrawAmount); 
         emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
         return balances[msg.sender];
     }
-
 }
